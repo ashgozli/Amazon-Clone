411ea87205b10b1901a9637bf3a04896701f81d5
ashgozli <ak23b24@gmail.com>
2022-12-26 03:12:38 -0800

    Added React Router v.5.2 to the application

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
 import Home from './Home';
 import Header from './Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      {/* Header: top bar */}
+    <Router>
       <div className="app">
-        <Header />
-        <Home />
+        <Switch>
+          <Route path='/checkout'>
+            <h1>I am checking out</h1>
+          </Route>
+          <Route path='/'>
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
       </div>
-
-      {/* Home */}
-    </div>
+    </Router>
   );
 }
 
