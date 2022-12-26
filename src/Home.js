fd7293dee9c03531164b516100a64368146328a1
ashgozli <ak23b24@gmail.com>
2022-12-26 02:55:07 -0800

    Impl all func and styling for home page products

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import Product from "./Product";
 
 function Home() {
    return (
@@ -9,19 +10,62 @@ function Home() {
                src="https://m.media-amazon.com/images/I/71JOx2u5kZL._SX3000_.jpg" alt="" />
 
             <div className="home_row">
-               {/* Products */}
-               {/* Products */}
-               {/* Products */}
-               {/* Products */}
+               <Product
+                  id="12245678"
+                  title='Anker Robotic Vaccums'
+                  image='https://m.media-amazon.com/images/I/41easFwemgL._AC_SY230_.jpg'
+                  price={399.99}
+                  rating={3}
+               />
+               <Product
+                  id="12245634"
+                  title='Xbox Series S Console'
+                  image='https://m.media-amazon.com/images/I/717JafCjCAL._AC_SX679_.jpg'
+                  price={379.98}
+                  rating={5}
+               />
+               <Product
+                  id="12248490"
+                  title='Samsung Galaxy S22'
+                  image='https://images-na.ssl-images-amazon.com/images/I/31Q+vZfIViL._AC_SX184_.jpg'
+                  price={987.99}
+                  rating={4}
+               />
             </div>
 
             <div className="home_row">
-               {/* Products */}
-               {/* Products */}
-               {/* Products */}
-               {/* Products */}
+               <Product
+                  id="12243143"
+                  title='Samsung 8K Neo QLED'
+                  image='https://images-na.ssl-images-amazon.com/images/I/414rCESiMnL._AC_SX184_.jpg'
+                  price={2998.99}
+                  rating={3}
+               />
+               <Product
+                  id="11113143"
+                  title='Samsung Galaxy Book2 Pro 360'
+                  image='https://images-na.ssl-images-amazon.com/images/I/31HjAiFpKhL._AC_SX184_.jpg'
+                  price={1599.99}
+                  rating={4}
+               />
+               <Product
+                  id="81183153"
+                  title='Apple Watch Series 8'
+                  image='https://m.media-amazon.com/images/I/71JGQoATh0L._AC_UL320_.jpg'
+                  price={569.99}
+                  rating={5}
+               />
             </div>
 
+            <div className="home_row">
+               <Product
+                  id="66243163"
+                  title='Samsung 49" Super Ultra-Wide'
+                  image='https://m.media-amazon.com/images/I/71RfMIBWkLL._AC_SX679_.jpg'
+                  price={1581.25}
+                  rating={5}
+               />
+            </div>
          </div>
       </div>
    );
--- src/Product.js
+++ src/Product.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import './Product.css';
+import React from "react";
+import "./Product.css";
 
-function Product() {
+function Product({ id, title, image, price, rating }) {
+
     return (
-        <div className='product'>
+        <div className="product">
+            <div className="product__info">
+                <p>{title}</p>
+                <p className="product__price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product__rating">
+                    {Array(rating)
+                        .fill()
+                        .map((_, i) => (
+                            <p>⭐</p>
+                        ))}
+                </div>
+            </div>
 
+            <img src={image} alt="" />
+
+            <button>Add to Basket</button>
         </div>
-    )
+    );
 }
 
-export default Product
+export default Product;
