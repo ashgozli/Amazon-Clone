309d39ee84fc71ea17b2085172388bdd3407d899
ashgozli <ak23b24@gmail.com>
2022-12-28 23:46:51 -0800

    Allow user to move back and for. from cart to home

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 // import SearchIcon from "@material-ui/icons/Search";
 // import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
@@ -6,11 +7,12 @@ import "./Header.css";
 function Header() {
     return (
         <div className="header">
-
-            <img
-                className="header__logo"
-                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-            />
+            <Link to="/">
+                <img
+                    className="header__logo"
+                    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                />
+            </Link>
 
             <div className="header__search">
                 <input className="header__searchInput" type="text" />
@@ -38,13 +40,14 @@ function Header() {
                     <span className="header__optionLineTwo">Prime</span>
                 </div>
 
-
-                <div className="header__optionBasket">
-                    {/* <ShoppingBasketIcon /> */}
-                    <span className="header__optionLineTwo header__basketCount">
-                        0
-                    </span>
-                </div>
+                <Link to="/checkout">
+                    <div className="header__optionBasket">
+                        {/* <ShoppingBasketIcon /> */}
+                        <span className="header__optionLineTwo header__basketCount">
+                            0
+                        </span>
+                    </div>
+                </Link>
 
             </div>
         </div>
