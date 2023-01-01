552a2b1c5737afcf00a5f4fce3cc74a36831387f
ashgozli <ak23b24@gmail.com>
2022-12-31 23:36:20 -0800

    Added username on the header when signed in

--- src/Header.js
+++ src/Header.js
@@ -32,7 +32,9 @@ function Header() {
             <div className="header__nav">
                 <Link to={!user && "/login"}>
                     <div onClick={handleAuthentication} className="header__option">
-                        <span className="header__optionLineOne">Hello Guest</span>
+                        <span className="header__optionLineOne">
+                            Hello {!user ? 'Guest' : user.email}
+                        </span>
                         <span className="header__optionLineTwo">
                             {user ? 'Sign Out' : 'Sign In'}
                         </span>
